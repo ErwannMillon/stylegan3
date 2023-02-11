dfcc3d5bf667d60474c7373e6074ea64092ed2d9
Erwann <erwannmillon@gmail.com>
2023-02-11 17:07:32 +0000

    add deployemnets

---
+++ charts/set_daemon.sh
@@ -0,0 +1,14 @@
+sudo tee /etc/docker/daemon.json <<EOF
+{
+    "default-runtime": "nvidia",
+    "runtimes": {
+        "nvidia": {
+            "path": "/usr/bin/nvidia-container-runtime",
+            "runtimeArgs": []
+        }
+    }
+}
+EOF
+sudo pkill -SIGHUP dockerd
+sudo systemctl daemon-reload
+sudo systemctl restart docker
